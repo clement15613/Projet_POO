6062c3b9048268361fdec638d1cf54caf1d3a503
clement15613 <93199116+clement15613@users.noreply.github.com>
2021-11-26 16:22:31 +0100

    vendredi26

--- Code_Projet/Code_Projet/Accueil.h
+++ Code_Projet/Code_Projet/Accueil.h
@@ -35,12 +35,17 @@ namespace CodeProjet {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ button3;
-	private: System::Windows::Forms::Button^ button4;
-	private: System::Windows::Forms::Button^ button5;
-	private: System::Windows::Forms::Button^ button6;
+
+	protected:
+
+
+
+
+
+
+
+
+
 	protected:
 
 	private:
@@ -56,91 +61,13 @@ namespace CodeProjet {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->button5 = (gcnew System::Windows::Forms::Button());
-			this->button6 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(12, 12);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(378, 229);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Personnel";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(396, 247);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(378, 229);
-			this->button2->TabIndex = 1;
-			this->button2->Text = L"Modifier mon compte";
-			this->button2->UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(12, 247);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(378, 229);
-			this->button3->TabIndex = 2;
-			this->button3->Text = L"Commande";
-			this->button3->UseVisualStyleBackColor = true;
-			// 
-			// button4
-			// 
-			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(780, 12);
-			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(378, 229);
-			this->button4->TabIndex = 3;
-			this->button4->Text = L"Stock";
-			this->button4->UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(396, 12);
-			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(378, 229);
-			this->button5->TabIndex = 4;
-			this->button5->Text = L"Client";
-			this->button5->UseVisualStyleBackColor = true;
-			// 
-			// button6
-			// 
-			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button6->Location = System::Drawing::Point(780, 247);
-			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(378, 229);
-			this->button6->TabIndex = 5;
-			this->button6->Text = L"Statistique";
-			this->button6->UseVisualStyleBackColor = true;
 			// 
 			// Accueil
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1242, 631);
-			this->Controls->Add(this->button6);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->Name = L"Accueil";
 			this->Text = L"Accueil";
